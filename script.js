afa3dab4f4254f995e33fa0a62dfc1cf36de003f
Mertcan <mertcgul@gmail.com>
2024-11-07 15:05:10 -0800

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Local Storage'dan verileri yükle
 document.querySelectorAll('.step textarea').forEach((textarea, index) => {
     const savedData = localStorage.getItem(`step${index}`);
     if (savedData) textarea.value = savedData;
 
-    // Veriyi Local Storage'a kaydet
     textarea.addEventListener('input', () => {
         localStorage.setItem(`step${index}`, textarea.value);
     });
+});
+
+// Submit butonu işlevi
+document.getElementById('submit-button').addEventListener('click', () => {
+    const restaurantName = document.getElementById('restaurant-name').value;
+    const observerName = document.getElementById('observer-name').value;
+    localStorage.setItem('restaurantName', restaurantName);
+    localStorage.setItem('observerName', observerName);
+    alert("Form data saved successfully!");
 });
